699b389c4dcd2e71c4e14f82a7ff0b666dce7ad9
DESKTOP-5FDTHUP\Cho <chokangjun@naver.com>
2021-11-22 15:43:37 +0900

    2884번 알림 시계

---
+++ BaekJoon/src/알람시계.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class 알람시계 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int H = scn.nextInt();
+		int M = scn.nextInt();
+
+		if(M<45) {
+			H--;
+			M = M+15;
+			if (H<0) {
+				H=23;
+			}
+			System.out.println(H + " " + M);
+		} else {
+			System.out.println(H + " " + (M-45));
+		}
+		scn.close();
+	}
+
+}
