f464e87bf21d28d9d6648abcaa2525355bc5bc88
DESKTOP-5FDTHUP\Cho <chokangjun@naver.com>
2021-11-22 15:03:00 +0900

    2753번 윤년

---
+++ BaekJoon/src/윤년.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class À±³â {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int year = scn.nextInt();
+		
+		if(year % 400 == 0) {
+			 System.out.println("1");
+		} else if (year % 100 == 0) {
+			System.out.println("0");
+		} else if (year % 4 == 0) {
+			System.out.println("1");	
+		} else {
+			System.out.println("0");
+		}
+		scn.close();
+
+	}
+
+}
