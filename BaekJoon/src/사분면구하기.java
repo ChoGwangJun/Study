c590800081849527ac2dbcae4d053bfa794f9ff0
DESKTOP-5FDTHUP\Cho <chokangjun@naver.com>
2021-11-22 15:10:32 +0900

    14681번 사분면구하기

---
+++ BaekJoon/src/사분면구하기.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class 사분면구하기 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int x = scn.nextInt();
+		int y = scn.nextInt();
+		
+		if (0<x && y>0) {
+			System.out.println("1");
+		} else if (0>x && 0<y) {
+			System.out.println("2");
+		} else if (0>x && 0>y) {
+			System.out.println("3");
+		} else if (0<x && 0>y) {
+			System.out.println("4");
+		}
+		scn.close();
+	}
+
+}
