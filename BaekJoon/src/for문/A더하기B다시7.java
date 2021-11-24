bec5b0bceffbfe7e092f1e9c50b89b669d64ae57
DESKTOP-5FDTHUP\Cho <chokangjun@naver.com>
2021-11-24 22:26:38 +0900

    11021번 A+B -7

---
+++ BaekJoon/src/for문/A더하기B다시7.java
@@ -0,0 +1,19 @@
+package for문;
+
+import java.util.Scanner;
+
+public class A더하기B다시7 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int T = scn.nextInt();
+
+		for (int i = 1; i <= T; i++) {
+			int A = scn.nextInt();
+			int B = scn.nextInt();
+			System.out.println("Case #" + i + ": " + (A+B));
+		}
+		scn.close();
+	}
+
+}
